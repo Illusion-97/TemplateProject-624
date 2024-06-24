779a95f3b3747260223dcb4e04fdd78ba229ab01
Yanis ADEKALOM <yadekalom@gmail.com>
2024-06-24 13:32:51 +0200

    Initial Commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "../views/home/home.component";
+import {LoginComponent} from "../views/login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  }
+];
